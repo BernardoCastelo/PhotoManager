2a2f7975292a31f628b6379f12ccf3de732c2ecb
Bernardo Castelo <bernardo.castelo@gmail.com>
2021-03-10 22:31:26 +0000

    Infocard now accepts all of the visual Chip's properties

--- photomanager-webapp/src/Components/Functional/InfoCard.js
+++ photomanager-webapp/src/Components/Functional/InfoCard.js
@@ -1,26 +1,32 @@
 import React from 'react'
 import Chip from '@material-ui/core/Chip';
 import { makeStyles } from '@material-ui/core/styles';
+import * as constants from '../../Constants/Constants';
 
 const useStyles = makeStyles({
   card: {
-    color: "white",
+    color: "wheat",
     opacity: '75%',
     fontSize: 'small',
-    marginLeft: '10px'
+    marginLeft: '10px',
+    width: '100%'
   }
 });
 
 const InfoCard = (props) => {
 
-  const background = props.background ? { background: props.background } : { background: "darkcyan" };
+  const background = props.background ? { background: props.background } : { background: constants.DARKSLATEGREY };
 
   const classes = useStyles();
 
   return (
-    <Chip className={classes.card} style={background}
-      variant='outlined'
+    <Chip className={classes.card}
+      color="primary"
+      style={background}
+      variant='default'
       label={props.label}
+      avatar={props.avatar}
+      icon={props.icon}
     />
   )
 }
