8b714708c7dd2be00c497aa84de070e7f3c064a3
Bernardo Castelo <bernardo.castelo@gmail.com>
2021-05-10 22:49:01 +0100

    Added Users DL

---
+++ DataLayer/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataLayer
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(IDbContainer dbContainer) : base(dbContainer)
+        {
+            this.DbContainer = dbContainer ?? throw new ArgumentNullException(nameof(dbContainer));
+        }
+    }
+}
